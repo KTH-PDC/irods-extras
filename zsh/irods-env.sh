2857b5890a1ced198cf55ac1c3b44edb4fd154db
Ilari Korhonen <ilarik@kth.se>
2016-11-30 17:34:02 +0100

    function irods-env from my .zshrc to be improved upon

---
+++ zsh/irods-env.sh
@@ -0,0 +1,28 @@
+# irods-env - a function to switch between different iRODS environments
+function irods-env {
+    env_name=$@
+    env_file="$HOME/.irods/irods_environment.json.$env_name"
+
+    if [ ! -f "$env_file" ]; then
+	echo "irods-env: unable to find iRODS environment '$env_name'"
+	return
+    fi
+
+    mv $HOME/.irods/irods_environment.json $HOME/.irods/irods_environment.json.backup
+    cp $HOME/.irods/irods_environment.json."$@" $HOME/.irods/irods_environment.json
+    iexit full
+
+    auth_scheme=`python -c "import os,json,sys; print json.load(open(os.path.join(os.getenv('HOME'), '.irods', 'irods_environment.json')))['irods_authentication_scheme']"`
+
+    if [ $auth_scheme = "KRB" ]; then
+	imiscsvrinfo
+    else
+	iinit; imiscsvrinfo
+    fi
+
+    if [ $? -ne "0" ]; then
+	printf "\nirods-env: unable to initialize iRODS environment '$@', reverting back!\n"
+	cp $HOME/.irods/irods_environment.json.backup $HOME/.irods/irods_environment.json
+	iinit
+    fi
+}
