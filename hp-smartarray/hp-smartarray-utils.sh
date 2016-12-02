d0b70d8e36a5669b1d805c5f48077b8abe0e3526
Ilari Korhonen <ilarik@kth.se>
2016-12-02 09:40:13 +0100

    fix POSIX shell compatibiity

--- hp-smartarray/hp-smartarray-utils.sh
+++ hp-smartarray/hp-smartarray-utils.sh
@@ -3,7 +3,23 @@
 
 HPBIN=hpacucli
 
-function array-create() {
+ctrl_set() { 
+    SLOT=$1
+    KEY=$2
+    VALUE=$3
+
+    $HPBIN ctrl slot=$SLOT modify $KEY=$VALUE
+}
+
+ctrl_show() { 
+    SLOT=$1
+    ARG2=$2
+    ARG3=$3
+    
+    $HPBIN ctrl slot=$SLOT show $ARG2 $ARG3
+}
+
+array_create() {
     SLOT=$1
     RAID=$2
     STRIPE=$3
@@ -12,28 +28,28 @@ function array-create() {
     $HPBIN ctrl slot=$SLOT create type=ld drives=$DRIVES raid=$RAID stripsize=$STRIPE
 }
     
-function array-show() {
+array_show() {
     SLOT=$1
     ID=$2
 
     $HPBIN ctrl slot=$SLOT ld $ID show
 }
 
-function array-delete() {
+array_delete() {
     SLOT=$1
     ID=$2
    
    $HPBIN ctrl slot=$SLOT ld $ID delete
 }
 
-function array-get-wwn() {
+array_get_wwn() {
     SLOT=$1
     ID=$2
 
     $HPBIN ctrl slot=$SLOT ld $ID show |grep Unique|awk '{print $3}'|awk '{print tolower($0)}'
 }
 
-function drive-show() {
+drive_show() {
     SLOT=$1
     ID=$2
 
